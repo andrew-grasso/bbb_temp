700562b1a9f5ed225ce7fa73ab2df8178f18827d
Andrew Grasso <andrewpgrasso@gmail.com>
2014-06-22 11:50:02 -0400

    [IMPL] Converted to class

--- temp.py
+++ temp.py
@@ -5,27 +5,32 @@ import time
 import math
 
 
-ADC.setup()
+class probe:
+    def __init__(self, adc_pin):
+        ADC.setup()
+        self.adc_pin = adc_pin
 
-def readTemp():
-    # From the datasheet
-    bvalue = 3950
-    r_bias = 10000
-    # solved using 10k for r_therm and 25 for temp_c
-    r_alpha = 0.017633
-    c_to_k = 273.15
+    def readTemp(self):
+        # From the datasheet
+        bvalue = 3950
+        r_bias = 10000
+        # solved using 10k for r_therm and 25 for temp_c
+        r_alpha = 0.017633
+        c_to_k = 273.15
 
-    adcValue = ADC.read("P9_36")
-    r_therm  = ((1/adcValue)-1)*r_bias
-    temp_c = (10*((bvalue/(math.log(r_therm/r_alpha)))-c_to_k))/10
-    return temp_c
+        adcValue = ADC.read(self.adc_pin)
+        r_therm  = ((1/adcValue)-1)*r_bias
+        temp_c = (10*((bvalue/(math.log(r_therm/r_alpha)))-c_to_k))/10
+        return temp_c
 
 def get_f(temp_c):
     temp_f = (temp_c * 9/5) + 32
     return temp_f
 
+myProbe = probe('P9_36')
+
 while True:
-    temp_c = readTemp()
+    temp_c = myProbe.readTemp()
     temp_f = get_f(temp_c)
     print 'temp_c=', temp_c, '  temp_f=', temp_f
     time.sleep(1)
